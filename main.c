ec7e85b487ef0db8ca30049ba927677e21c70176
Artur Chaves <artur7paiva@gmail.com>
2024-05-02 13:12:54 +0000

    Inicio menu

---
+++ main.c
@@ -0,0 +1,25 @@
+// #include <stdio.h>
+// #include "functions.h"
+
+
+
+// int main(){
+
+//     function functions[] = {novo_cliente, deletar_cliente, listar_cliente, debito, deposito, extrato, transacao};
+
+
+
+
+//     Conta contas[TOTAL];
+//     int pos = 0;
+
+
+
+
+
+
+
+
+
+
+// }
