f67c990db2d8d3beb472fddbf63537e140180d49
A.Leall <83127864+oLeall99@users.noreply.github.com>
2024-05-09 21:52:28 +0000

    Criação de Menu e estruturação das funções

--- functions.c
+++ functions.c
@@ -3,13 +3,39 @@
 #include <ctype.h>
 #include "functions.h"
 
-Value novo_cliente(Conta contas[], int *pos){};     // Função de Criar um novo cliente
-Value deletar_cliente(Conta contas[], int *pos) {};   // Função de Deletar um cliente
-Value listar_cliente(Conta contas[], int *pos) {};    // Função de Listar os clientes
-Value debito(Conta contas[], int *pos) {};    // Função de debitar dinheiro de uma conta
-Value deposito(Conta contas[], int *pos) {};  // Função de depositar um valor em uma conta
-Value extrato(Conta contas[], int *pos) {}; // Função de gerar extrato do cliente
-Value transacao(Conta contas[], int *pos) {}; // Função de realizar transacao entre contas
 
+
+Value novo_cliente(Conta contas[], int *pos, int *user){};     // Função de Criar um novo cliente
+Value deletar_cliente(Conta contas[], int *pos, int *user) {}; // Função de Deletar um cliente
+Value listar_cliente(Conta contas[], int *pos, int *user) {};  // Função de Listar os clientes
+Value debito(Conta contas[], int *pos, int *user) {};          // Função de debitar dinheiro de uma conta
+Value deposito(Conta contas[], int *pos, int *user) {};        // Função de depositar um valor em uma conta
+Value extrato(Conta contas[], int *pos, int *user) {};         // Função de gerar extrato do cliente
+Value transacao(Conta contas[], int *pos, int *user) {};       // Função de realizar transacao entre contas
+Value salvar(Conta contas[], int *pos, int *user);             // Função de Salvar a lista de contatos no arquivo binário
+Value carregar(Conta contas[], int *pos, int *user);           // Função de Carregar a lista de contatos do arquivo binário
+Value hash(char senha[], int *pos);
+Value hash_compare(char hash[], char senha[], int *pos);
 void clearBuffer() {}; // Função de Limpeza de Buffer
 void tratarRes(Value err) {}; // Tratamento de Erros das funções de tipo Value
+
+
+
+int menu_login(Conta contas[], int *pos){
+    int opcao;
+    
+    do{
+        printf("\n\033[34m|==================================|\n");
+        printf("|- - -| Banco Quem Poupa Tem |- - -|\n");
+        printf("|==================================|\n");
+        printf("| > Menu de Login:                 |\n");
+        printf("|   1 - Fazer Login                |\n");
+        printf("|   2 - Fazer Cadastro             |\n");
+        printf("|   0 - Sair                       |\n");
+        printf("|==================================|\n");
+        printf("| > Escolha uma opcao: ");
+        scanf("%d", &opcao);
+        //clearBuffer();
+        
+    }while(opcao != 0);
+}
--- functions.h
+++ functions.h
@@ -1,32 +1,45 @@
 #define TOTAL 1000
-#define T_NOME 300
-#define T_CPF 11 
+#define T_NOME 100
+#define T_CPF 12 
 #define SENHA 100
+#define T_EXTRATO 100
 
+typedef struct {
+    int tipo; // 1 = Deposito / 2 = Débito / 3 = Transferência Receber / 4 = Trasnferência Pagar
+    int data;
+    float valor;
+
+} Extrato;
 
 typedef struct {
     char cpf[T_CPF];
     char nome[T_NOME];
-    int tipo_conta;
-    float Saldo;
     char senha[SENHA];
+    int tipo_conta; // 1 = Comum / 2 = Plus
+    Extrato extrato[T_EXTRATO]; 
+    float Saldo;
 } Conta;
 
-typedef enum {OK, MAX_CONTAS, SEM_CONTAS, ABRIR, FECHAR, ESCREVER, LER, CRIAR} Value;
+typedef enum { OK, ABRIR, FECHAR, ESCREVER, LER, CRIAR, MAX_CONTATO, SEM_CONTATO, AUTENTICACAO } Value;
 
 typedef Value (*func)(Conta[], int*);
-Value novo_cliente(Conta contas[], int *pos);     // Função de Criar um novo cliente
-Value deletar_cliente(Conta contas[], int *pos);   // Função de Deletar um cliente
-Value listar_cliente(Conta contas[], int *pos);    // Função de Listar os clientes
-Value debito(Conta contas[], int *pos);    // Função de debitar dinheiro de uma conta
-Value deposito(Conta contas[], int *pos);  // Função de depositar um valor em uma conta
-Value extrato(Conta contas[], int *pos); // Função de gerar extrato do cliente
-Value transacao(Conta contas[], int *pos); // Função de realizar transacao entre contas
 
-void clearBuffer(); // Função de Limpeza de Buffer
-void tratarRes(Value err); // Tratamento de Erros das funções de tipo Value
+Value novo_cliente(Conta contas[], int *pos, int *user){};     // Função de Criar um novo cliente
+Value deletar_cliente(Conta contas[], int *pos, int *user) {}; // Função de Deletar um cliente
+Value listar_cliente(Conta contas[], int *pos, int *user) {};  // Função de Listar os clientes
+Value debito(Conta contas[], int *pos, int *user) {};          // Função de debitar dinheiro de uma conta
+Value deposito(Conta contas[], int *pos, int *user) {};        // Função de depositar um valor em uma conta
+Value extrato(Conta contas[], int *pos, int *user) {};         // Função de gerar extrato do cliente
+Value transacao(Conta contas[], int *pos, int *user) {};       // Função de realizar transacao entre contas
+Value salvar(Conta contas[], int *pos, int *user);             // Função de Salvar a lista de contatos no arquivo binário
+Value carregar(Conta contas[], int *pos, int *user);           // Função de Carregar a lista de contatos do arquivo binário
+Value hash(char senha[], int *pos);
+Value hash_compare(char hash[], char senha[], int *pos);
 
-// \033[0m -> Cor Padrão de texto
-// \033[34m -> Cor azul  - Utilizado no menu
-// \033[32m -> Cor Verde - Utilizado para mensagens de sucesso de funções
-// \033[31m -> Cor Vermelha - Utilizado para mensagens de erro
+//Funções auxiliares
+
+int menu_login(Conta contas[], int *pos);   // Menu de Login
+void menu_adm(Conta contas[], int *pos);            // Menu de Administrador
+void menu_usuario(Conta contas[], int *pos);        // Menu de Usuário
+void clear_buffer();        // Limpeza de Buffer
+void tratarRes(Value err);  // Tratamento de Erros
