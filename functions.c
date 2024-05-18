125c0a2d84460a86fd8a150b30ad51d80445b2c7
Artur Chaves <artur7paiva@gmail.com>
2024-05-18 13:01:01 -0300

    Introdução ao salvamento de arquivos com as novas duas funções de arquivos para extrato

--- functions.c
+++ functions.c
@@ -369,6 +369,7 @@ void clearBuffer() { // Função de Limpeza de Buffer
 
 // Funções de Arquivo:
 // Manipulação do Arquivo binário
+
 Value salvar(
     Conta contas[],
     int *pos) { // Função de Salvar a lista de contatos no arquivo binário
@@ -391,13 +392,13 @@ Value salvar(
   return OK;
 }
 Value carregar(
-    Conta contatos[],
+    Conta contas[],
     int *pos) { // Função de Carregar a lista de contatos do arquivo binário
   FILE *f = fopen("contas.bin", "rb");
   if (f == NULL)
     return ABRIR;
 
-  int qtd = fread(contatos, TOTAL, sizeof(Conta), f);
+  int qtd = fread(contas, TOTAL, sizeof(Conta), f);
   if (qtd == 0)
     return LER;
 
@@ -411,6 +412,51 @@ Value carregar(
   printf("\033[32m| > Contas carregados!!\n");
   return OK;
 }
+
+Value salvar_extrato(
+    Extrato extratos[],
+    int *pos) { // Função de Salvar a lista de contatos no arquivo binário
+  FILE *arq = fopen("extratos.bin", "wb");
+  if (arq == NULL)
+    return ABRIR;
+
+  int qtd = fwrite(extratos, T_EXTRATO, sizeof(Extrato), arq);
+  if (qtd == 0)
+    return ESCREVER;
+
+  qtd = fwrite(pos, 1, sizeof(int), arq);
+  if (qtd == 0)
+    return ESCREVER;
+
+  if (fclose(arq))
+    return FECHAR;
+
+  printf("\033[32m| > Extrato Salvo!!\n");
+  return OK;
+}
+
+
+
+carregar_extrato(
+  Extrato extratos[],
+  int *pos){
+    FILE *f = fopen("extrato.bin", "rb");
+    if (f == NULL)
+      return ABRIR;
+
+    int qtd = fread(extratos, T_EXTRATO, sizeof(Extrato), f);
+    if (qtd == 0)
+      return LER;
+
+    if (fclose(f))
+      return FECHAR;
+
+    printf("\033[32m| > Extratos carregados!!\n");
+
+  }
+
+
+
 
 int findCPF(Conta contas[], int pos, const char *cpf) {
   for (int i = 0; i < pos; i++) {
